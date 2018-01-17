824ab47f6b4fe2473e27d41d3ff4934bc40b6175
Akram Abdelnasser <akram71093@hotmail.com>
2018-01-17 10:29:52 +0200

    restricting access to features

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -4,21 +4,30 @@ require "rails_helper"
 RSpec.feature "Showing an Article" do
 
   before do
-    john = User.create(email: "john@example.com" , password: "password")
-    login_as(john)
-    @article = Article.create(title: "This is a title", body: "This is a body", user: john)
+    @john = User.create(email: "john@example.com" , password: "password")
+    @article = Article.create(title: "This is a title", body: "This is a body", user: @john)
+    @fred = User.create(email: "fred@example.com" , password: "password")
+
+
   end
 
 
-  scenario "A user shows an article" do
+  scenario "to a non-signed in user hides Edit/Delete Links" do
     visit "/"
-
     click_link @article.title
-
     expect(page).to have_content(@article.title)
     expect(page).to have_content(@article.body)
-
     expect(current_path).to eq(article_path(@article))
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+
+  scenario "A non-owner signed in cannot see both links" do
+    login_as(@fred)
+    visit "/"
+    click_link @article.title
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
   end
 
 end
