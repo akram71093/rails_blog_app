e5ee84118af66f6dcbfc49dbba5723bbee581adb
Akram Abdelnasser <akram71093@hotmail.com>
2018-01-17 09:39:48 +0200

    Fixing specs

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -4,7 +4,9 @@ require "rails_helper"
 RSpec.feature "Showing an Article" do
 
   before do
-    @article = Article.create(title: "This is a title", body: "This is a body")
+    john = User.create(email: "john@example.com" , password: "password")
+    login_as(john)
+    @article = Article.create(title: "This is a title", body: "This is a body", user: john)
   end
 
 
